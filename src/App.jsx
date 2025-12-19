902d789ebdc61d107c73ca84d7054dde6dfb9a62
harininr <145009229+harininr@users.noreply.github.com>
2025-12-19 12:05:58 +0530

    Deploy React portfolio

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Hi, I'm Harini 👋</h1>
+      <p>This is my React portfolio</p>
+    </div>
+  )
+}
+
+export default App
